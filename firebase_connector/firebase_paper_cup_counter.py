d4a1e727f3f5c95eb1b1ba88d7f54eb1046e30ba
Nathan Waddington <Nathan.Waddington@akqa.com>
2015-08-06 10:07:43 -0700

    adding error handling to connections. oops.

--- firebase_connector/firebase_paper_cup_counter.py
+++ firebase_connector/firebase_paper_cup_counter.py
@@ -1,13 +1,17 @@
 __author__ = 'Nate Waddington'
 __email__ = 'nathan.waddington@akqa.com'
 
+import requests
 from firebase import firebase
 import datetime
 
 def firebase_post(json):
-    _firebase = firebase.FirebaseApplication('https://papercup22.firebaseio.com', None)
+    try:
+        _firebase = firebase.FirebaseApplication('https://papercup22.firebaseio.com', None)
 
-    today = datetime.datetime.now()
-    url = '/paper_cup_count/' + str(today).split(' ')[0]
-    result = _firebase.post(url, json)
-    # if debug: print result
+        today = datetime.datetime.now()
+        url = '/paper_cup_count/' + str(today).split(' ')[0]
+        result = _firebase.post(url, json)
+        # if debug: print result
+    except requests.exceptions.ConnectionError as e:
+        print str(e)
