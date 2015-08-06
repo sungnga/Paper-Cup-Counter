7ee74eb8fd00d7e76aab6123b0b04e9f36a47222
Nathan Waddington <Nathan.Waddington@akqa.com>
2015-08-05 18:27:10 -0700

    formatting firebase post: including date.

--- firebase_connector/firebase_paper_cup_counter.py
+++ firebase_connector/firebase_paper_cup_counter.py
@@ -2,9 +2,12 @@ __author__ = 'Nate Waddington'
 __email__ = 'nathan.waddington@akqa.com'
 
 from firebase import firebase
+import datetime
 
 def firebase_post(json):
     _firebase = firebase.FirebaseApplication('https://papercup22.firebaseio.com', None)
 
-    result = _firebase.post('/paper_cup_count', json)
-    if debug: print result
+    today = datetime.datetime.now()
+    url = '/paper_cup_count/' + str(today).split(' ')[0]
+    result = _firebase.post(url, json)
+    # if debug: print result
